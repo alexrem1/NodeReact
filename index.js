5001bdd1454fd86f10e2a97105b5a81d1534ff6b
alexrem1 <remidy@live.co.uk>
2021-06-02 12:32:59 +0100

    Access and refresh tokens

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken) => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Access Token:", accessToken);
+      console.log("refresh Token:", refreshToken);
+      console.log("profile:", profile);
     }
   )
 );
