07ff20a913f0abb06b95bf9836e16c64ca5f15a9
alexrem1 <remidy@live.co.uk>
2021-06-03 10:55:01 +0100

    Mongoose Model Classes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
 require("./services/passport");
+require("./models/user");
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
