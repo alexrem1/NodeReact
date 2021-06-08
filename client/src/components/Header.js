27a7bc224b124fdb76aa185b02159a0b72eaeaf2
alexrem1 <remidy@live.co.uk>
2021-06-08 02:03:39 +0100

    Header Design

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,19 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h2>Header</h2>
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">
+            <li>
+              <a>Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
